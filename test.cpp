f4d32b627df0f3704f7d74ebb35b7463b5bbd8a9
Nanahoshi74 <yukitakegl@gmail.com>
2023-11-15 14:53:50 +0900

    resize で配列サイズを変形することに変更した

--- test.cpp
+++ test.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 int ITEM_NUM, MAX_WEIGHT;//アイテムの個数と重さ
-vector<int> itemWeight(ITEM_NUM), itemValue(ITEM_NUM);
+vector<int> itemWeight, itemValue;
 
-int calculate_idealValue(){
-
+int calculate_idealValue(void){
+    return 1;
 }
 
 int main(){
 
     cin >> ITEM_NUM >> MAX_WEIGHT;//アイテムの個数と重さの限界を入力
+    itemWeight.resize(ITEM_NUM), itemValue.resize(ITEM_NUM);
 
     for(int i = 0; i < ITEM_NUM; i++){
         cin >> itemWeight[i] >> itemValue[i];
