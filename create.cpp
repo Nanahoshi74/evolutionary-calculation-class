784bfb08fa499df79c22e8d9f6eba5f29ee3ac6d
Nanahoshi74 <yukitakegl@gmail.com>
2023-12-18 14:20:58 +0900

    テストケース作成用の関数を作成した

---
+++ create.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int seed = 0;
+
+long long get_rand_range(long long min_val, long long max_val) {
+    // 乱数生成器
+    static mt19937 mt(seed);
+
+    // [min_val, max_val] の一様分布整数 (int) の分布生成器
+    uniform_int_distribution<long long> get_rand_uni_int( min_val, max_val );
+
+    // 乱数を生成
+    return get_rand_uni_int(mt);
+}
+
+int main(){
+    long long n = get_rand_range(1, 100);
+    long long w = get_rand_range(1, 1e5);
+    cout << n << " " << w << endl;
+    for(int i = 0; i < n; i++){
+        long long W = get_rand_range(1, w);
+        long long V = get_rand_range(1, 1e9);
+        cout << W << " " << V << endl;
+    }
+
+    return 0;
+}
